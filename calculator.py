8779f35c6496e35609ae2cc823a502bce63a5a8d
Ada Gutan <adagutan@gmail.com>
2023-03-23 19:35:56 +0200

    işlevsiz

--- calculator.py
+++ calculator.py
@@ -4,14 +4,11 @@ def calculate():
     expression=()
     try:
         result = eval(expression)
-        log+= " ", result
-
-        return result and log
+        return result
     except:
         return "Invalid expression. Please enter a valid arithmetic expression."
-    
-    
-    
+
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
 
@@ -22,12 +19,14 @@ main.title("Clean Calculator")
 frame = ctk.CTkFrame(master=main)
 frame.pack(pady=20,padx=40,fill='both',expand=True)
 
-expression = ctk.CTkEntry(master=frame,placeholder_text="Enter")
+expression = ctk.CTkEntry(master=frame,placeholder_text="Input Expression")
 expression.pack(pady=15,padx=10, fill='both',expand=True)
 
 lilframe = ctk.CTkFrame(master=main)
 lilframe.pack(pady=20,padx=40)
 
+label = ctk.CTkLabel(master=lilframe,text="log",width=120,height=25,corner_radius=8,font=('Roboto', 14))
+label.pack(pady=20,padx=40,fill='both')
 
 
 main.mainloop()
